ab23b9a0af38242106619992b4cd08b1faff83bc
Akshat Agrawal <adroit@Akshats-MacBook-Air.local>
2021-04-15 17:02:35 +0530

    Added scenario for a spare and score of all ones

--- src/test/java/org/bowling/tdd/demo/BowlingGameTest.java
+++ src/test/java/org/bowling/tdd/demo/BowlingGameTest.java
@@ -2,17 +2,34 @@ package org.bowling.tdd.demo;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class BowlingGameTest {
 
+	BowlingGame game = null;
+	
+	@BeforeEach
+	public void initialize() {
+		game = new BowlingGame();
+	}
+	
 	@Test
 	public void computeScoreForGameOfAllOnes() {
-		BowlingGame game = new BowlingGame();
 		for (int i = 0; i < 20; i++) {
 			game.calculateTotalNumberOfPinsDown(1);
 		}
 		assertEquals(20, game.getTotalScore());
+	}
+	
+	@Test
+	public void computeScoreForGameWithSpareAndRestAllOnes() {
+		game.calculateTotalNumberOfPinsDown(5);
+		game.calculateTotalNumberOfPinsDown(5);
+		for (int i = 2; i <= 20; i++) {
+			game.calculateTotalNumberOfPinsDown(1);
+		}
+		assertEquals(29, game.getTotalScore());
 	}
 
 }
